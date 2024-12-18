32e0151f215b2c856788113aa7c07b44741905dd
YuliaLantzberg <lanzyuka@gmail.com>
2024-12-18 11:21:47 +0200

    Add sidebar to the mobile view. Add script for the sidebar

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburgerBtn = document.querySelector(".humburger");
+const closeBtn = document.querySelector(".menu__close");
+
+function openNav() {
+	const menu = document.querySelector(".menu");
+	document.querySelector(".overlay").style.display = "block";
+	menu.style.display = "flex";
+	menu.style.width = "68.26%";
+	document.body.style.overflowY = "hidden";
+}
+
+function closeNav() {
+	const menu = document.querySelector(".menu");
+	document.querySelector(".overlay").style.display = "none";
+	menu.style.display = "none";
+	menu.style.width = "0";
+	document.body.style.overflowY = "scroll";
+}
+
+hamburgerBtn.addEventListener("click", openNav);
+closeBtn.addEventListener("click", closeNav);
